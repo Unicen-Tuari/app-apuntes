281e4793f86ae674695ef78a3308212533f4c3fe
Alejandro Perez <alecio88@gmail.com>
2019-09-17 19:09:22 -0300

    Adds Dusk

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CUYS
                 </div>
 
                 <div class="links">
